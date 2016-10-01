a2c844bf73a3e20cfa737ee2ada4ad3c27a3e0b3
Christopher Roland <brianroland@Christophers-Mac-mini.local>
2016-10-01 13:58:13 -0400

    BR: finished problem 42

--- mongan_42/functions_and_classes_42/sl_list.h
+++ mongan_42/functions_and_classes_42/sl_list.h
@@ -1,6 +1,9 @@
 //
 // Created by Christopher Roland on 9/30/16.
 //
+// Found it useful to make decision diagram for each method,
+// depending on the values of arguments and state of object --> different types of outcome
+//
 
 #ifndef MONGAN_SL_LIST_H
 #define MONGAN_SL_LIST_H
@@ -8,78 +11,167 @@
 template <class T>
 class SingleLinkNode {
 private:
-    const T *data;             //instructions using data cannot change value at that address
-    SingleLinkNode<T> *next;
+    const T *data_;             //instructions using data cannot change value at that address
+    SingleLinkNode<T> *next_;
 public:
-    SingleLinkNode(const T *data_, SingleLinkNode *next_) : data(data_), next(next_) {}
+    SingleLinkNode(const T *data, SingleLinkNode *next) : data_(data), next_(next) {}
     ~SingleLinkNode() {
         printf("SingleLinkNode:~SingleLinkNode(): beg\n");
-        delete this->data;              //the node is responsible for the memory at data
+        delete this->data_;              //the node is responsible for the memory at data
         printf("SingleLinkNode:~SingleLinkNode(): end\n");
     }
 
-    SingleLinkNode *getNext() const {
-        return next;
+    SingleLinkNode<T> * next() const {
+        return next_;
     }
 
-    void setNext(SingleLinkNode *next) {
-        SingleLinkNode::next = next;
+    void set_next(SingleLinkNode * next) {
+        SingleLinkNode::next_ = next;
     }
 
-    const T *getData() const {
-        return data;
+    const T * data() const {
+        return data_;
     }
+
+
+};
+
+template<class T>
+struct FindResult {
+    bool found_;
+    const SingleLinkNode<T> * target_;
+    const SingleLinkNode<T> * previous_;
+    FindResult(bool found, const SingleLinkNode<T> * target, const SingleLinkNode<T> * previous)
+            : found_(found), target_(target), previous_(previous) {}
 };
 
 template <class T>
 class SinglyLinkedList {
 private:
-    SingleLinkNode<T> *head;
-    SingleLinkNode<T> *tail;
+    SingleLinkNode<T> *head_;
+    SingleLinkNode<T> *tail_;
 
 public:
-    SinglyLinkedList(): head(nullptr), tail(nullptr) {}
+    SinglyLinkedList(): head_(nullptr), tail_(nullptr) {}
 
-    bool isEmpty() { return head==nullptr;}
-    const T * front(){
-        if (this->isEmpty() ){
+    bool IsEmpty() const { return head_==nullptr;}
+    const T * GetDataAtFront() const {
+        if (this->IsEmpty() ){
             return nullptr;
         } else {
-            return this->head->getData();
+            return this->head_->data();
         }
     }
 
-    bool insertAfter(SingleLinkNode<T> * target, const T *data) {
+    bool InsertAfter(SingleLinkNode<T> * target, const T * data) {
 
-        //insert at head
-        if (target==nullptr and head==nullptr) {
-            head = new SingleLinkNode<T>(data, nullptr);
-            tail = head;
+        //force insert at head
+        if (target == nullptr and head_ == nullptr) {
+            head_ = new SingleLinkNode<T>(data, nullptr);
+            tail_ = head_;
             return true;
-        } else if (target==nullptr and head!=nullptr) {
-            head = new SingleLinkNode<T>(data, head->getNext());
+        } else if (target == nullptr and head_ != nullptr) {
+            head_ = new SingleLinkNode<T>(data, head_);
             return true;
         }
 
-        //insert not at head
-        SingleLinkNode<T> * curr = head;
+        //target is not nullptr, do not force insert at head, looking for a valid address to insert after
+        SingleLinkNode<T> * curr = head_;
         while (true) {
-            if (curr==target) {
-                target->setNext( new SingleLinkNode<T>(data, target->getNext()) );
-                if (target==tail) {
-                    tail = target->getNext();
+            if (curr==nullptr) {
+                return false;
+            } else if (curr==target) {
+                curr->set_next( new SingleLinkNode<T>(data, curr->next()) );
+                if (curr==tail_) {
+                    tail_ = curr->next();
                 }
                 return true;
-            } else if (curr==nullptr) {
-                return false;
-            }else {
-                curr = curr->getNext();
+
+            } else {
+                curr = curr->next();
             }
         }
     }
 
+    bool InsertInFront(const T * data) {
+        return InsertAfter(nullptr, data);
+    }
 
+    bool Remove(SingleLinkNode<T> * target) {
 
+        if (target==nullptr){
+            return false;
+        } else if (this->IsEmpty()) {
+            return false;
+        } else {
+
+            SingleLinkNode<T> * curr = head_;
+            SingleLinkNode<T> * prev = head_;
+
+            while (true) {
+
+                if (curr==nullptr) {
+                    return (false);
+
+                } else if (curr==target) {
+
+                    if (curr==head_ and curr==tail_) {
+                        head_ = nullptr;
+                        tail_ = nullptr;
+                    } else if (curr==head_ and curr!=tail_) {
+                        head_ = curr->next();
+                    } else if (curr!=head_ and curr==tail_) {
+                        tail_ = prev;
+                        prev->set_next(nullptr);
+                    } else {
+                        prev->set_next(curr->next());
+                    }
+
+                    delete curr;
+                    curr=nullptr;
+                    return(true);
+
+                } else {
+                    prev = curr;
+                    curr = curr->next();
+                }
+            }
+        }
+
+    }
+
+    FindResult<T> * Find(const T * t) {
+
+        if (t==nullptr ) {
+            return new FindResult<T>(false, nullptr, nullptr);
+        } else if (this->IsEmpty() ) {
+            return new FindResult<T>(false, nullptr, nullptr);
+        } else {
+
+            SingleLinkNode<T> * curr = head_;
+            SingleLinkNode<T> * prev = nullptr;
+
+            while (true) {
+
+                if (curr==nullptr) {
+                    return new FindResult<T>(false, nullptr, nullptr);
+                } else if (curr->data()==t) {
+                    return new FindResult<T>(true, curr, prev);
+                } else {
+                    prev = curr;
+                    curr = curr->next();
+                }
+            }
+        }
+    }
+
+    SingleLinkNode<T> * head() const {
+        return head_;
+    }
+
+    SingleLinkNode<T> * tail() const {
+        return tail_;
+    }
 };
 
 
--- mongan_42/tests_42/basic_tests_42/test_mongan_42.cpp
+++ mongan_42/tests_42/basic_tests_42/test_mongan_42.cpp
@@ -12,7 +12,7 @@ TEST(test_mongan_42, hello_world) {
 TEST(test_mongan_42, create_destroy_single_link_node) {
     printf("\ncreate_destroy_single_link_node: beg\n");
     SingleLinkNode<double> *node = new SingleLinkNode<double>(new double(1.23), nullptr);
-    printf("data=%.2f\n", *node->getData());
+    printf("data=%.2f\n", *node->data());
     delete node;
     printf("create_destroy_single_link_node: end\n");
 }
@@ -22,21 +22,53 @@ TEST(test_mongan_42, insert_at_head) {
 
     //test that a single insertion results in the value put at the front
     SinglyLinkedList<double> * list = new SinglyLinkedList<double>();
-    list->insertAfter(nullptr, new double(1.23));
-    ASSERT_EQ(*list->front(), 1.23);
+    list->InsertAfter(nullptr, new double(1.23));
+    ASSERT_EQ(*list->GetDataAtFront(), 1.23);
 
     //test that a second insertion results in the second value at the front
-    list->insertAfter(nullptr, new double(2.34));
-    ASSERT_EQ(*list->front(), 2.34);
-
+    list->InsertAfter(nullptr, new double(2.34));
+    ASSERT_EQ(*list->GetDataAtFront(), 2.34);
 
     //test that inserting after an address not in the data structure does not change the value of front
     SingleLinkNode<double> * singleton_data = new SingleLinkNode<double>(new double(3.45), nullptr);
-    list->insertAfter(singleton_data, new double(4.56));
-    ASSERT_EQ(*list->front(), 2.34);
+    list->InsertAfter(singleton_data, new double(4.56));
+    ASSERT_EQ(*list->GetDataAtFront(), 2.34);
 
     //check the value of is_empty
-    ASSERT_EQ(list->isEmpty(),false);
+    ASSERT_EQ(list->IsEmpty(),false);
 
     printf("insert_at_head: end\n");
+}
+
+TEST(test_mongan_42, remove_from_head) {
+    printf("\nremove_at_head: beg\n");
+    SinglyLinkedList<double> * list = new SinglyLinkedList<double>();
+    list->InsertInFront(new double(1.23));
+    ASSERT_EQ(*list->GetDataAtFront(), 1.23);
+    ASSERT_EQ(list->Remove(list->head()), true);
+    ASSERT_EQ(list->IsEmpty(), true);
+    ASSERT_EQ(list->head(), nullptr);
+    ASSERT_EQ(list->tail(), nullptr);
+    printf("remove_at_head: end\n");
+}
+
+TEST(test_mongan_42, remove_from_middle) {
+    printf("\nremove_from_middle: beg\n");
+    SinglyLinkedList<double> * list = new SinglyLinkedList<double>();
+    double * a = new double(1.23);
+    double * b = new double(4.56);
+    double * c = new double(7.89);
+
+
+    list->InsertInFront(a);
+    list->InsertInFront(b);
+    list->InsertInFront(c);
+    ASSERT_EQ(*list->GetDataAtFront(), *c);
+
+    FindResult<double> * find_result_b = list->Find(b);
+    ASSERT_EQ(find_result_b->found_, true);
+    const SingleLinkNode<double> * middle = find_result_b->target_;
+    ASSERT_EQ(middle->data(),b);
+
+    printf("remove_from_middle: end\n");
 }
