c30e64e18f9405ddafdfe13d904e51b0a506f2e0
Christopher Roland <brianroland@Christophers-Mac-mini.local>
2016-10-01 16:30:30 -0400

    BR: tested height function

--- mongan_51/functions_and_classes_51/tree_height.h
+++ mongan_51/functions_and_classes_51/tree_height.h
@@ -9,17 +9,34 @@
 
 template<class T>
 struct Node{
-    Node * left;
-    Node * right;
-    T * data;
+    Node<T> * left_;
+    Node<T> * right_;
+    T * data_;
+    Node(T * data, Node<T> * left=nullptr, Node<T> * right=nullptr)
+            : data_(data), left_(left), right_(right) {}
 };
 
-int height(const Node * root) {
+int MaxInt(int a, int b) {
+    return (b>a) ? b : a;
+}
+
+template<class T>
+int height(const Node<T> * root) {
     assert(root!=nullptr);
 
-    
+    int height_via_left_subtree=0;
+    int height_via_right_subtree=0;
 
-    return 1;
+    if (root->left_!=nullptr) {
+        height_via_left_subtree = 1 + height(root->left_);
+    }
+    if (root->right_!=nullptr) {
+        height_via_right_subtree = 1 + height(root->right_);
+    }
+
+    int larger_height = MaxInt(height_via_left_subtree, height_via_right_subtree);
+
+    return larger_height;
 }
 
 
--- mongan_51/tests_51/basic_tests_51/test_mongan_51.cpp
+++ mongan_51/tests_51/basic_tests_51/test_mongan_51.cpp
@@ -3,8 +3,33 @@
 //
 
 #include <iostream>
+#include <functions_and_classes_51/tree_height.h>
 #include "gtest/gtest.h"
 
 TEST(test_mongan_51, hello_world) {
     printf("\nhello world\n");
+}
+
+TEST(test_mongan_51, test_max_int) {
+    printf("\ntest_max_int: beg\n");
+    ASSERT_EQ(5, MaxInt(3,5));
+    ASSERT_EQ(5, MaxInt(5,3));
+    printf("test_max_int: end\n");
+}
+
+
+TEST(test_mongan_51, test_height) {
+    printf("\ntest_height: beg\n");
+
+    Node<double> * e = new Node<double>(new double(10));
+    Node<double> * d = new Node<double>(new double(20));
+    Node<double> * c = new Node<double>(new double(30), e, nullptr);
+    Node<double> * b = new Node<double>(new double(40));
+    Node<double> * a = new Node<double>(new double(50), c, d);
+    Node<double> * root = new Node<double>(new double(60), a, b);
+
+    ASSERT_EQ(height(e), 0);
+    ASSERT_EQ(height(c), 1);
+    ASSERT_EQ(height(root), 3);
+    printf("test_height: end\n");
 }
